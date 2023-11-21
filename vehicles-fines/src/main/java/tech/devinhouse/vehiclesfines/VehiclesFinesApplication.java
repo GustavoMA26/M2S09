30c86cc9a1ed805f3e957104524386bc4e0e3f87
Gustavo <gmalmeida26@gmail.com>
2023-11-21 12:36:01 -0300

    Initial commit

---
+++ vehicles-fines/src/main/java/tech/devinhouse/vehiclesfines/VehiclesFinesApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.vehiclesfines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesFinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesFinesApplication.class, args);
+	}
+
+}
---
+++ vehicles-fines/src/test/java/tech/devinhouse/vehiclesfines/VehiclesFinesApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.vehiclesfines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesFinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
